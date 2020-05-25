b7c5f695d41958267f35f434c532cb5af254b6e4
Elena <anel92r@gmail.com>
2020-05-25 16:48:22 +0200

    add links to the main page

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -25,17 +25,31 @@ const LinksContainer = styled.div`
   @media only screen and (max-width: 500px) {
     width: 90%;
   }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
 
 const Links = (props) => {
   return (
     <div>
       <LinksContainer>
-        <FontAwesomeIcon icon={faGithub} />
-        <FontAwesomeIcon icon={faLinkedin} />
-        <FontAwesomeIcon icon={faFileAlt} />
-        <FontAwesomeIcon icon={faAt} />
-        <FontAwesomeIcon icon={faInstagram} />
+        <a href="https://github.com/elenaserezhkina">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://www.linkedin.com/in/olena-shutovska/">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="https://drive.google.com/file/d/1tB8dpAu2zDzNCNlkTiIvrkzztK1fc1kS/view?usp=sharing">
+          <FontAwesomeIcon icon={faFileAlt} />
+        </a>
+        <a href="mailto:o.shutovska@gmail.com">
+          <FontAwesomeIcon icon={faAt} />
+        </a>
+        <a href="https://www.instagram.com/elenaserezhkina/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
       </LinksContainer>
     </div>
   );
